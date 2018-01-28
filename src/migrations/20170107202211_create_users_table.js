37ca62e3738174198922e6e28f09e311bc234adc
Sanjay Shiwakoti <sanjayshiwakoti@lftechnology.com>
2018-01-28 17:03:07 +0545

    file initialized.

---
+++ src/migrations/20170107202211_create_users_table.js
@@ -0,0 +1,28 @@
+/**
+ * Create users table.
+ *
+ * @param  {object} knex
+ * @return {Promise}
+ */
+export function up(knex) {
+  return knex.schema.createTable('auth.auth_users', table => {
+    table.uuid('id');
+    table
+      .timestamp('created_at')
+      .notNull()
+      .defaultTo(knex.raw('now()'));
+    table.timestamp('updated_at').notNull();
+    table.string('username').notNull();
+    table.string('password').notNull();
+  });
+}
+
+/**
+ * Drop users table.
+ *
+ * @param  {object} knex
+ * @return {Promise}
+ */
+export function down(knex) {
+  return knex.schema.dropTable('users');
+}
