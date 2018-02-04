6f608e0348eb3d5a13fcfee227b70c8ef2d94957
Sanjay Shiwakoti <sanjayshiwakoti@lftechnology.com>
2018-02-04 11:47:00 +0545

    UUID and sessions.

--- src/migrations/20170107202211_create_users_table.js
+++ src/migrations/20170107202211_create_users_table.js
@@ -6,7 +6,7 @@
  */
 export function up(knex) {
   return knex.schema.createTable('auth_users', table => {
-    table.uuid('id');
+    table.uuid('id').primary();
     table
       .timestamp('created_at')
       .notNull()
