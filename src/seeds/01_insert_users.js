2c922904bd412b527d3699c0815f6bc5e1692b53
Sanjay Shiwakoti <sanjayshiwakoti@lftechnology.com>
2018-02-04 12:38:55 +0545

    remove delete from seed.

--- src/seeds/01_insert_users.js
+++ src/seeds/01_insert_users.js
@@ -8,20 +8,18 @@ import uuid from 'uuid/v4';
  */
 export function seed(knex, Promise) {
   // Deletes all existing entries
-  return knex('auth_users')
-    .del()
-    .then(() => {
-      return Promise.all([
-        // Inserts seed entries
-        knex('auth_users').insert([
-          {
-            id: uuid(),
-            username: 'rosia',
-            password: 'rosia',
-            created_at: new Date(),
-            updated_at: new Date()
-          }
-        ])
-      ]);
-    });
+  return knex('auth_users').then(() => {
+    return Promise.all([
+      // Inserts seed entries
+      knex('auth_users').insert([
+        {
+          id: uuid(),
+          username: 'rosia',
+          password: 'rosia',
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ])
+    ]);
+  });
 }
